2d1095acae5e7588bc3bcb66bea6df930021e234
Henrique Gusso <henrique@gus.so>
2019-01-17 23:55:50 +0000

    Hide child components from public

--- code/_EmptyState.tsx
+++ code/_EmptyState.tsx
@@ -21,6 +21,68 @@ export default ({ size, initial, event }: Props) => {
   const shouldShowArrow = hasAvailableHeight && width >= arrowWidth + 6
   const shouldShowTitle = hasAvailableHeight && shouldShowArrow
 
+  const Title: React.SFC = ({ children }) => (
+    <span
+      style={{
+        flex: 'auto',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textAlign: 'center',
+        fontSize: 12,
+        WebkitMaskImage:
+          'linear-gradient(90deg, black, black calc(100% - 12px), transparent)',
+      }}
+    >
+      {children}
+    </span>
+  )
+
+  const ChildType: React.SFC<{ connected: number }> = ({
+    children,
+    connected = false,
+  }) => (
+    <span
+      style={{
+        padding: `2px 5px 3px`,
+        margin: 5,
+        borderRadius: 2,
+        border: `1px solid rgba(${color}, .4)`,
+
+        background: !connected && `rgba(${color}, .9)`,
+        color: !connected && `rgba(255,255,255,.8)`,
+      }}
+    >
+      {children}
+    </span>
+  )
+
+  const Arrow = () => {
+    const width = 14
+    const height = 7
+
+    return (
+      <svg
+        viewBox={`0 0 ${width} ${height}`}
+        style={{ flexShrink: 0, width: width, marginTop: 1 }}
+      >
+        <g transform="translate(0.5 0.5)">
+          <path
+            d="M 0 3 L 12 3"
+            fill="transparent"
+            stroke={`rgb(${color})`}
+            strokeLinecap="butt"
+          />
+          <path
+            d="M 9 0 L 12 3 L 9 6"
+            fill="transparent"
+            stroke={`rgb(${color})`}
+            strokeLinecap="butt"
+          />
+        </g>
+      </svg>
+    )
+  }
+
   return (
     <Frame
       width={width}
@@ -45,67 +107,5 @@ export default ({ size, initial, event }: Props) => {
       )}
       {shouldShowArrow && <Arrow />}
     </Frame>
-  )
-}
-
-const Title: React.SFC = ({ children }) => (
-  <span
-    style={{
-      flex: 'auto',
-      whiteSpace: 'nowrap',
-      overflow: 'hidden',
-      textAlign: 'center',
-      fontSize: 12,
-      WebkitMaskImage:
-        'linear-gradient(90deg, black, black calc(100% - 12px), transparent)',
-    }}
-  >
-    {children}
-  </span>
-)
-
-const ChildType: React.SFC<{ connected: number }> = ({
-  children,
-  connected = false,
-}) => (
-  <span
-    style={{
-      padding: `2px 5px 3px`,
-      margin: 5,
-      borderRadius: 2,
-      border: `1px solid rgba(${color}, .4)`,
-
-      background: !connected && `rgba(${color}, .9)`,
-      color: !connected && `rgba(255,255,255,.8)`,
-    }}
-  >
-    {children}
-  </span>
-)
-
-const Arrow = () => {
-  const width = 14
-  const height = 7
-
-  return (
-    <svg
-      viewBox={`0 0 ${width} ${height}`}
-      style={{ flexShrink: 0, width: width, marginTop: 1 }}
-    >
-      <g transform="translate(0.5 0.5)">
-        <path
-          d="M 0 3 L 12 3"
-          fill="transparent"
-          stroke={`rgb(${color})`}
-          strokeLinecap="butt"
-        />
-        <path
-          d="M 9 0 L 12 3 L 9 6"
-          fill="transparent"
-          stroke={`rgb(${color})`}
-          strokeLinecap="butt"
-        />
-      </g>
-    </svg>
   )
 }
